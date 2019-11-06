6185004c7abbf343e854d242f8966db385c359b2
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2019-11-05 21:40:36 -0500

    heroku mongoDB URI added

--- controllers/dbConnection.js
+++ controllers/dbConnection.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose")
 const dbDebugger = require("debug")("app:dataBaseConnection");
 module.exports = async function () {
     try {
-        await mongoose.connect("mongodb://localhost/Booksy", {
+        await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Booksy", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
         });
         dbDebugger("app is connected to MONGO DB OLGY")
+        console.log(process.env.MONGODB_URI);
     } catch (error) {
         dbDebugger("there was an error connecting to MONGO DB OLGY: ", error);
     }
