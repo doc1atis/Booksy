f317071e889ba90f1ffbe96d2b9a846703e0d23a
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2019-11-08 22:56:17 -0500

    new home page styling added

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const startUp = require("./controllers/startup")
 const portDebugger = require("debug")("app:portConnection")
 const app = express();
 startUp(app)
+app.get("/homey", (req, res) => {
+  res.render("homey")
+})
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   portDebugger("app is listening on port ", port);
--- models/UserModel.js
+++ models/UserModel.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 const passportLocalMongoose = require("passport-local-mongoose");
 const userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    purchases: [{ type: mongoose.Schema.Types.ObjectId, ref: "book" }],
+    cart: [{ type: mongoose.Schema.Types.ObjectId, ref: "book" }],
+    wishList: [{ type: mongoose.Schema.Types.ObjectId, ref: "book" }]
 });
 userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model("User", userSchema);
