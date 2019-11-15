d9d39bf92889734636bfd5a8113177db19f5c229
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2019-11-15 15:06:55 -0500

    mogo atals added

--- controllers/dbConnection.js
+++ controllers/dbConnection.js
@@ -1,15 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const dbDebugger = require("debug")("app:dataBaseConnection");
-module.exports = async function () {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Booksy", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-        dbDebugger("app is connected to MONGO DB OLGY")
-    } catch (error) {
-        dbDebugger("there was an error connecting to MONGO DB OLGY: ", error);
-    }
-}
-
+module.exports = async function() {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_ATLAS_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    dbDebugger("app is connected to MONGO DB OLGY");
+  } catch (error) {
+    dbDebugger("there was an error connecting to MONGO DB OLGY: ", error);
+  }
+};
