21f8ad0810af58f13f6e66edff75229ffd070e75
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2019-11-15 16:15:18 -0500

    revert to mongo lab

--- controllers/dbConnection.js
+++ controllers/dbConnection.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 const dbDebugger = require("debug")("app:dataBaseConnection");
 module.exports = async function() {
   try {
-    await mongoose.connect(process.env.MONGO_DB_ATLAS_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    });
-    console.log("app is connected to MONGO DB OLGY");
+    await mongoose.connect(
+      process.env.MONGO_DB_URI || process.env.MONGO_DB_ATLAS_URI,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      }
+    );
     dbDebugger("app is connected to MONGO DB OLGY");
   } catch (error) {
-    console.log("there was an error connecting to MONGO DB OLGY: ", error);
     dbDebugger("there was an error connecting to MONGO DB OLGY: ", error);
   }
 };
