c20e3a48beb76abbc37b39fc9615dd2bcfc1557f
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2019-11-19 23:34:54 -0500

    download book functionality added

--- routes/downloadRouter.js
+++ routes/downloadRouter.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const fs = require("fs");
+const s3s = require("s3-streams");
+const AWS = require("aws-sdk");
+AWS.config.region = "us-east-1";
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_Access_Key_ID,
+  secretAccessKey: process.env.AWS_Secret_Access_Key
+});
 const router = express.Router();
 router.get("/", (req, res) => {
-  // get file path from mongodb using the book ID,
-  const filePath = "path of file from mongodb";
-  const file = fs.createReadStream(filePath);
-  res.setHeader("Content-Type", "application/pdf");
-  res.setHeader("Content-Disposition", `inline; filename= ${filePath}`);
-  file.pipe(res);
+  const downloader = s3s.ReadStream(s3, {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: req.query.bookContent.split("com/")[1]
+  });
+  downloader.pipe(res);
 });
 module.exports = router;
